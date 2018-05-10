751cc4ecdfd75e7edd4e7bb3654f20df781b82b5
Luis Rosales <eng.luisrosales@gmail.com>
2018-05-11 01:11:19 +0200

    adding html for render a recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'A test maybe', 'https://www.iha.com/residence-and-castle-for-rent-jimbaran_59925')
+  ];
+
   constructor() { }
 
   ngOnInit() {
