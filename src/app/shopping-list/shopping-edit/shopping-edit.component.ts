3dbdad4af1c10eef19f92508522fb02c10296263
Luis Rosales <eng.luisrosales@gmail.com>
2018-05-17 02:21:59 +0200

    component comunication end

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,7 +10,18 @@ export class ShoppingEditComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChild('nameInput') ingredientName: ElementRef;
+  @ViewChild('amountInput') ingredientAmount: ElementRef;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   ngOnInit() {
+  }
+
+  addIngredient() {
+    const ingredient = new Ingredient(this.ingredientName.nativeElement.value, 
+                                      this.ingredientAmount.nativeElement.value);
+    this.ingredientAdded.emit(ingredient);
   }
 
 }
