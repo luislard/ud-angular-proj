30dfb053ddb80aed829eb2aaa9f65a4c48f35a47
Luis Rosales <eng.luisrosales@gmail.com>
2018-05-18 23:42:05 +0200

    adding ingredients to recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,30 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-        new Recipe('Hola elias es pato', 'A test maybe', 'http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
-        new Recipe('Elias es mariquito', 'A test maybe', 'http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg')
+        new Recipe(
+            'Hola elias es pato',
+            'A test maybe',
+            'http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg',
+            [
+                new Ingredient('azucar', 2),
+                new Ingredient('leche', 1),
+            ]
+        ),
+        new Recipe(
+            'Elias es mariquito',
+            'A test maybe',
+            'http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg',
+            [
+                new Ingredient('cafe', 2),
+                new Ingredient('mantequilla', 1),
+            ]
+        )
       ];
 
     getRecipes() {
