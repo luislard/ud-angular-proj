cab7158e8e7da588107bf929972f041d8d304294
Luis Rosales <eng.luisrosales@gmail.com>
2018-05-24 00:25:23 +0200

    removing unnecessary import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
