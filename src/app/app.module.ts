5b4e89bcb0952d507de1b9ff444bdef44829a300
Luis Rosales <eng.luisrosales@gmail.com>
2018-05-24 23:00:12 +0200

    adding router links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeDefaultComponent } from './recipes/recipe-default/recipe-default.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { RecipeDefaultComponent } from './recipes/recipe-default/recipe-default.
     ShoppingEditComponent,
     DropdownDirective,
     RecipeDefaultComponent,
+    RecipeEditComponent,
   ],
   imports: [
     BrowserModule,
